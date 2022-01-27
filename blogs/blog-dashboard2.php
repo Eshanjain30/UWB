7042a23f33e0c3d97ee5390ab26605bee38349c8
aniketbanik-sapien <aniket.banik012@nmims.edu.in>
2022-01-28 00:31:44 +0500

    blog data retrieved successfully(#pending done)

--- blogs/blog-dashboard2.php
+++ blogs/blog-dashboard2.php
@@ -7,10 +7,33 @@
     if(!$conn){
         die("Connection failed: " . mysqli_connect_error());
     }
+
+    ?>
+
+<?php
+    if(isset($_SESSION['success']))
+	{
+		echo $_SESSION['success'];
+		unset($_SESSION['success']);
+	}
+	?>
+
+    <?php
+    $ubid = $_GET['id'];
     $sql = "select * from blog order by date ASC";
+    
     $result = $conn->query($sql) or die($conn->error);
-    session_start();
-   
+    $res=mysqli_query($conn,$sql);
+    $row = mysqli_fetch_array($res);
+    
+	// $bid=$_GET['bid'];
+	// $query="select * from blog where user_id='$user_id'";
+	// $res=mysqli_query($conn,$query);
+	// $data=mysqli_fetch_array($res);
+
+    $myqu = "select * from blog where `bid`='$ubid'";
+    $mysql = mysqli_query($conn,$myqu);
+    $myrow = mysqli_fetch_array($mysql);
      ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,12 +41,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blogs | </title>
+    <title>Blogs | Dashboard</title>
     <link rel="shortcut icon" type="image/x-icon" href="../common_images/favicon.png">
     <link rel="stylesheet" href="../common_styles/navbar.css">
     <link rel="stylesheet" href="blog-dashboard-style.css">
 </head>
-
 <body>
     <div class="menu_icon flex-col">
         <div class="bars"></div>
@@ -36,11 +58,13 @@
             <div class="navigation_bl flex-col">
                 <div class="searchbar flex-row">
                     <input type="search" name="" id="search_blogs" placeholder="search topic, date, user">
-                    <a href="#query"><i class="fas fa-search"></i></a>
+                    <button><i class="fas fa-search"></i></button>
                 </div>
                 <div class="options flex-row">
-                    <a href="#pending" class="active_list">Pending</a>
-                    <a href="#rev">Reviewed</a>
+                    <!-- <button class="active_list">Pending</button>
+                    <button>Reviewed</buttton> -->
+                    <a href="#pending "class="active_list">Pending</a>
+                    <a href="#rev">Reviewed</a>        
                 </div>
             </div>
             <div class="lists flex-row">
@@ -63,26 +87,45 @@
                     <li class="d3 datewise">
                         <div class="older dates">older</div>
                         <div class="older_blogs pending_blogs">
-                            <!-- // Card Format for Outline -->
-                            <?php 
+
+                        <?php 
                             while($row=mysqli_fetch_assoc($result))
                             {
                                 ?>
                             <div class="blog_card flex-col">
                                 <div class="a_ttl"><?php echo $row['btitle'] ?></div>
                                 <div class="a_det">by <span class="a_name"><?php echo $row['uname'] ?></span>, <span class="a_date"><?php echo $row['date']?></span></div>
-                                <a href="blog-dashboard2.php?id=<?php echo $row['bid'];?>">read more</a>
+                                <a href="blog-dashboard2.php?id=<?php echo $row['bid']?>">read more</a>
                             </div>
                             <?php
                             }
                             ?>
-                        </form>
-            
+                            <!-- <div class="blog_card flex-col">
+                                <div class="a_ttl">new blogs</div>
+                                <div class="a_det">by <span class="a_name">someone blogger</span>, <span class="a_date">25 december</span></div>
+                                <a href="blog-dashboard2.html">read more</a>
+                            </div>
+                            <div class="blog_card flex-col">
+                                <div class="a_ttl">new blogs</div>
+                                <div class="a_det">by <span class="a_name">someone blogger</span>, <span class="a_date">25 december</span></div>
+                                <a href="blog-dashboard2.html">read more</a>
+                            </div>
+                            <div class="blog_card flex-col">
+                                <div class="a_ttl">new blogs</div>
+                                <div class="a_det">by <span class="a_name">someone blogger</span>, <span class="a_date">25 december</span></div>
+                                <a href="blog-dashboard2.html">read more</a>
+                            </div>
+                            <div class="blog_card flex-col">
+                                <div class="a_ttl">new blogs</div>
+                                <div class="a_det">by <span class="a_name">someone blogger</span>, <span class="a_date">25 december</span></div>
+                                <a href="blog-dashboard2.html">read more</a>
+                            </div> -->
 
                         </div>
                     </li>
 
                 </ul>
+
                 <ul class="reviewed_li bl_list">
                     <li class="result_blogs">
                         
@@ -91,7 +134,7 @@
                             <div class="flex-col">
                                 <div class="a_ttl">new blog</div>
                                 <div class="a_det">by <span class="a_name">someone blogger</span>, <span class="a_date">25 december</span></div>
-                                <a href="#rdm">read more</a>
+                                <a href="blog-dashboard2.php">read more</a>
                             </div>
                         </div>
 
@@ -130,7 +173,8 @@
                     <button type="submit" class="publish_b">Publish</button>
                     <button type="reset" class="cancel_b">Cancel</button>
                 </div>
-             </form>
+                
+            </form>
         </div>
 
         <div class="select_post_par flex-row inactive2">
@@ -140,20 +184,18 @@
             </div>
         </div>
         <div class="sucs inactive2">Success !</div>
-        <div class="right_wrap flex-col inactive2">
+        <div class="right_wrap flex-col inactive22">
             <div class="blog_complete flex-col">
-
                 <div class="top flex-col">
-                    <div class="ttl_page"></div>
-                    
-                    <div class="auth_page">by <span class="authname_page"></span>, <span class="authdate_page"></span></div>
+                    <div class="ttl_page"><?php echo $myrow['btitle'];?></div>
+                    <div class="auth_page">by <span class="authname_page"><?php echo $myrow['uname'];?></span>, <span class="authdate_page"><?php echo $myrow['date']; ?></span></div>
                     <div class="desc_page">
-                        
+                        <?php echo $myrow['bdescription']; ?>
                     </div>
                 </div>
-                
                 <div class="story_page">
-                    <p class="main_story">story
+                    <p class="main_story">
+                        <?php echo $myrow['bstory'];?>
                     </p>
                 </div>
             </div>
